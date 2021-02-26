5dfc10bade6b377bd9492a391ebca00c886d990d
Alejandrodk <ottoabarriosp@hotmail.com>
2021-02-26 18:58:27 -0300

    Feature: Create orderProduct relational entity

---
+++ src/main/java/com/market/Ecommerceapp/persistence/entity/OrderProduct.java
@@ -0,0 +1,31 @@
+package com.market.Ecommerceapp.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "compras_productos")
+public class OrderProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "id_compra")
+    private Integer orderId;
+
+    @Column(name = "id_producto")
+    private Integer productId;
+
+    @Column(name = "cantidad")
+    private Integer quantity;
+
+    @Column(name = "estado")
+    private String status;
+
+    private BigDecimal total;
+}
