43277cc28a03c4b681f821266286a72decd89703
alejandrodk <ottoabarriosp@hotmail.com>
2021-02-26 14:09:38 -0300

    initial commit

---
+++ src/test/java/com/market/Ecommerceapp/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.Ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
