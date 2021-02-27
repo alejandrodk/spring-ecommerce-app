a6822beb2dd8e3558e47fc798f998b168b7cc7f3
Alejandrodk <ottoabarriosp@hotmail.com>
2021-02-27 19:48:51 -0300

    Feature: Use Spring Query Methods in product repository

--- src/main/java/com/market/Ecommerceapp/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/market/Ecommerceapp/persistence/crud/ProductCrudRepository.java
@@ -3,4 +3,11 @@ package com.market.Ecommerceapp.persistence.crud;
 import com.market.Ecommerceapp.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductCrudRepository extends CrudRepository<Product, Integer> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByCategoryIdOrderByNameAsc(int categoryId);
+
+    Optional<List<Product>> findByStockLessThanAndStatus(int stock, boolean status);
+}
--- src/main/java/com/market/Ecommerceapp/persistence/repository/ProductRepository.java
+++ src/main/java/com/market/Ecommerceapp/persistence/repository/ProductRepository.java
@@ -3,12 +3,23 @@ package com.market.Ecommerceapp.persistence.repository;
 import com.market.Ecommerceapp.persistence.crud.ProductCrudRepository;
 import com.market.Ecommerceapp.persistence.entity.Product;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory(int categoryId) {
+        return productCrudRepository.findByCategoryIdOrderByNameAsc(categoryId);
+    }
+
+    public List<Product> getByStockAndStatus(int stock, boolean status) {
+        Optional<List<Product>> result = productCrudRepository.findByStockLessThanAndStatus(stock, status);
+        return result.orElse(Collections.emptyList());
     }
 }
