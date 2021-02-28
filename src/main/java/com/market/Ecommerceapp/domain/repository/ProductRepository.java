670ca34c74523c29485f8e405b873f48f5e6e54f
Alejandrodk <ottoabarriosp@hotmail.com>
2021-02-27 21:52:46 -0300

    Feature: Define product repository interface

---
+++ src/main/java/com/market/Ecommerceapp/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.market.Ecommerceapp.domain.repository;
+
+import com.market.Ecommerceapp.domain.dal.ProductDAL;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<ProductDAL> getAll();
+    Optional<List<ProductDAL>> getByCategory(int categoryId);
+    Optional<List<ProductDAL>> getScaseProducts(int stockQuantity);
+    Optional<ProductDAL> getProduct(int productId);
+    ProductDAL create(ProductDAL product);
+    void delete(int productId);
+}
