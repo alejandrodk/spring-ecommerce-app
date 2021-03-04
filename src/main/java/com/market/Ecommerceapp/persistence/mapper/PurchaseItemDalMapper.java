1e70e6301ee47c43d71f50d37f6b9854ae197cf1
Alejandrodk <ottoabarriosp@hotmail.com>
2021-03-03 23:46:09 -0300

    Feature: Create pucharse item mapper

---
+++ src/main/java/com/market/Ecommerceapp/persistence/mapper/PurchaseItemDalMapper.java
@@ -0,0 +1,21 @@
+package com.market.Ecommerceapp.persistence.mapper;
+
+import com.market.Ecommerceapp.domain.dal.PurchaseItemDAL;
+import com.market.Ecommerceapp.persistence.entity.OrderProduct;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = { ProductDalMapper.class })
+public interface PurchaseItemDalMapper {
+    @Mappings({
+            @Mapping(source = "status", target = "active")
+    })
+    PurchaseItemDAL toPurchaseItem(OrderProduct product);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "order", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    OrderProduct toOrderProduct(PurchaseItemDAL purchaseItem);
+}
