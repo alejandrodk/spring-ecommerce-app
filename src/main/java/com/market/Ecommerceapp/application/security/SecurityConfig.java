0e5cf6c99ba244f78d92b6e0e815b0b2531b901d
Alejandrodk <ottoabarriosp@hotmail.com>
2021-03-07 19:35:10 -0300

    Feature: Disable Spring session creation

--- src/main/java/com/market/Ecommerceapp/application/security/SecurityConfig.java
+++ src/main/java/com/market/Ecommerceapp/application/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/auth").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
