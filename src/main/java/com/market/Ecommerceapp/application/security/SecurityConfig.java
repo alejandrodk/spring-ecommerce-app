6467c375d0256885e9ce63e309a10faa3f1df8fe
Alejandrodk <ottoabarriosp@hotmail.com>
2021-03-07 18:27:53 -0300

    Feature: Implement Spring Security

---
+++ src/main/java/com/market/Ecommerceapp/application/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.market.Ecommerceapp.application.security;
+
+import com.market.Ecommerceapp.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
